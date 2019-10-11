394fabd4d8c8734b7ecf463db6d6576919f43a7e
rodicadp <56158034+rodicadp@users.noreply.github.com>
2019-10-11 13:24:40 +0300

    primul exemplu

---
+++ html/ec2.js
@@ -0,0 +1,27 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2019.10.11.0";
+
+function solve()
+{
+    var a=document.getElementById("id_a").value;
+    var b=document.getElementById("id_b").value;
+    var c=document.getElementById("id_c").value;
+    var delta = b * b - 4 * a * c;
+    var x1_re, x2_re, x1_im, x2_im;
+
+        if(delta >= 0)
+        {
+            x1_re=(-b - Math.sqrt(delta)) / (2*a);
+            x2_re=(-b + Math.sqrt(delta)) / (2*a);
+            x1_im = 0;
+            x2_im = 0;
+        }
+        else {
+                x1_re= -b / (2*a);
+                x2_re= -b / (2*a);
+                x1_im =(- Math.sqrt(-delta)) / (2*a);
+                x2_im=(+ Math.sqrt(-delta)) / (2*a);
+
+        }
+        document.getElementById("id_x1").innerHTML = x1_re + "+" + x1_im + "i";
+        document.getElementById("id_x2").innerHTML = x2_re + "+" + x2_im + "i";
+}
