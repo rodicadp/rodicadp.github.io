ae01518f8fc38fb3c7f2239299889c01d092c421
rodicadp <56158034+rodicadp@users.noreply.github.com>
2019-10-18 15:03:18 +0300

    modificare fisier

--- html/gyro/gyro.js
+++ html/gyro/gyro.js
@@ -1,7 +1,7 @@
 window.addEventListener("deviceorientation", on_rotate_uab);
 function on_rotate_uab(e)
 {
-    document.getElementById("id_alfa").innerHTML = e.alfa;
+    document.getElementById("id_alfa").innerHTML = e.alpha;
     document.getElementById("id_beta").innerHTML = e.beta;
     document.getElementById("id_gamma").innerHTML = e.gamma;
 
