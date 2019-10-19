fbeee71f02ecbce692a3cbe2c00b1cee95a83dfc
rodicadp <56158034+rodicadp@users.noreply.github.com>
2019-10-19 16:55:21 +0300

    Add files via upload

--- html/gyro/gyro.js
+++ html/gyro/gyro.js
@@ -1,8 +1,8 @@
 window.addEventListener("deviceorientation", on_rotate_uab);
+
 function on_rotate_uab(e)
 {
-    document.getElementById("id_alfa").innerHTML = e.alpha;
-    document.getElementById("id_beta").innerHTML = e.beta;
-    document.getElementById("id_gamma").innerHTML = e.gamma;
-
+	document.getElementById("id_alpha").innerHTML = e.alpha;
+	document.getElementById("id_beta").innerHTML = e.beta;
+	document.getElementById("id_gamma").innerHTML = e.gamma;
 }
