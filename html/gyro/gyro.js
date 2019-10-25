3401db0d398d59dc4fbeb66cc6363ca56bbbbbc7
rodicadp <56158034+rodicadp@users.noreply.github.com>
2019-10-25 14:08:50 +0300

    Add files via upload

--- html/gyro/gyro.js
+++ html/gyro/gyro.js
@@ -1,18 +1,18 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2019.10.25.0 ";
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2019.10.25.1 ";
 
 window.addEventListener("deviceorientation", on_orientation_uab);
 window.addEventListener("devicemotion", on_motion_uab);
 
 function on_orientation_uab(e)
 {
-	document.getElementById("id_alpha").innerHTML = e.alpha;
-	document.getElementById("id_beta").innerHTML = e.beta;
-	document.getElementById("id_gamma").innerHTML = e.gamma;
+	document.getElementById("id_alpha").innerHTML = Math.round(e.alpha * 100)/100;
+	document.getElementById("id_beta").innerHTML = Math.round(e.beta * 100)/100;
+	document.getElementById("id_gamma").innerHTML = Math.round(e.gamma * 100)/100;
 }
 
 function on_motion_uab(e)
 {
-	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
-	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
-	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
+	document.getElementById("id_acc_z").innerHTML = Math.round(e.accelerationIncludingGravity.z * 100)/100;
+	document.getElementById("id_acc_x").innerHTML = Math.round(e.accelerationIncludingGravity.x * 100)/100;
+	document.getElementById("id_acc_y").innerHTML = Math.round(e.accelerationIncludingGravity.y * 100)/100;
 }
