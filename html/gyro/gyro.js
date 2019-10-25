79fec078bd2496bc70f4edfe61e1b3f13d92c97e
rodicadp <56158034+rodicadp@users.noreply.github.com>
2019-10-25 15:09:38 +0300

    Add files via upload

--- html/gyro/gyro.js
+++ html/gyro/gyro.js
@@ -3,11 +3,26 @@ document.getElementById("id_logic_version").innerHTML = "Logic version = 2019.10
 window.addEventListener("deviceorientation", on_orientation_uab);
 window.addEventListener("devicemotion", on_motion_uab);
 
+function desenare(beta, gamma)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.beginPath();
+	var r = 10;
+	var x = canvas.width / 2 + beta / 90 * (canvas.width / 2 - r);
+	var y = canvas.height / 2 + gamma / 90 * (canvas.height / 2 - r);;
+	context.arc(x, y, r, 0, 2 * Math.PI);
+	context.stroke();
+
 function on_orientation_uab(e)
 {
 	document.getElementById("id_alpha").innerHTML = Math.round(e.alpha * 100)/100;
 	document.getElementById("id_beta").innerHTML = Math.round(e.beta * 100)/100;
 	document.getElementById("id_gamma").innerHTML = Math.round(e.gamma * 100)/100;
+
+	//desenare(e.beta,e.gamma);
 }
 
 function on_motion_uab(e)
@@ -17,6 +32,11 @@ function on_motion_uab(e)
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc.x * 100)/100;
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc.y * 100)/100;
 
-	document.getElementById("id_beta_acc").innerHTML = -Math.round(Math.atan(acc.x / acc.z) / Math.PI * 180* 100)/100;
-	document.getElementById("id_gamma_acc").innerHTML = Math.round(Math.atan(acc.y / acc.z) / Math.PI * 180* 100)/100;
+	var beta = -Math.atan(acc.y / acc.z) / Math.PI * 180;
+	var beta = Math.atan(acc.x / acc.z) / Math.PI * 180;
+	document.getElementById("id_beta_acc").innerHTML = Math.round(beta * 100)/100;
+	document.getElementById("id_gamma_acc").innerHTML = Math.round(gamma * 100)/100;
+
+	desenare(beta, gamma);
+
 }
