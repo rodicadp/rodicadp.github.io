086df70f0266385a02be7db7c6f25c66a62931b8
rodicadp <56158034+rodicadp@users.noreply.github.com>
2019-11-29 13:22:34 +0200

    Add files via upload

--- html/touch/touch.js
+++ html/touch/touch.js
@@ -4,7 +4,17 @@ window.addEventListener("touchstart", touch_start_uab);
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
+
 var client_rect = canvas.getBoundingClientRect();
+
+function get_random_color()
+{
+    vat tmp = "0123456789ABCDEF";
+    var culoare = "#";
+    for (var i = 0; i < 6; i++)
+        culoare += tmp[Math.floor(Math.random() * 16)];
+    return culoare;
+}
 
 
 function touch_start_uab(e)
@@ -14,6 +24,8 @@ function touch_start_uab(e)
     {
         context.beginPath();
         context.arc(t[i].pageX - client_rect.left, t[i].pageY - client_rect.top, 10, ,0, 2 * Math.PI);
+        context.fillStyle = get_random_color();
+        context.strokeStyle = context.fillStyle;
         context.stroke();
     }
 }
