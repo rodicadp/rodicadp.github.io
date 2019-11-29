7e8bd193c7915fa07f0f80a7ca602aadb5e776e3
rodicadp <56158034+rodicadp@users.noreply.github.com>
2019-11-29 13:10:20 +0200

    Add files via upload

--- html/touch/touch.js
+++ html/touch/touch.js
@@ -1,9 +1,10 @@
-document.getElementById("id_logic_version").innerHTML = "2019.11.29.0";
+document.getElementById("id_logic_version").innerHTML = "2019.11.29.1";
 
 window.addEventListener("touchstart", touch_start_uab);
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
+var client_rect = canvas.getBoundingClientRect();
 
 
 function touch_start_uab(e)
@@ -12,7 +13,7 @@ function touch_start_uab(e)
     for (var i = 0; i < t.length; i++)
     {
         context.beginPath();
-        context.arc(t[i].pageX, t[i].pageY, 10, ,0, 2 * Math.PI);
+        context.arc(t[i].pageX - client_rect.left, t[i].pageY - client_rect.top, 10, ,0, 2 * Math.PI);
         context.stroke();
     }
 }
